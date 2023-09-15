87bf862b9114c7c50762bfdda72f950f55d99383
Neha_Deore <nehadeore160@gmail.com>
2023-09-15 11:58:20 +0530

    quote task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// export default App;
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState('');
+  const [quoteList, setQuoteList] = useState([]);
+
+  // Function to fetch a random quote
+  const fetchRandomQuote = async () => {
+    try {
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      const randomQuote = response.data.slip.advice;
+      setQuote(randomQuote);
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+    }
+  };
+
+  // Function to add the current quote to the list of fetched quotes
+  const addQuoteToList = () => {
+    if (quote) {
+      setQuoteList([...quoteList, quote]);
+      setQuote('');
+    }
+  };
+
+  // Use useEffect to fetch a quote when the component mounts
+  useEffect(() => {
+    fetchRandomQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Random Advice Generator</h1>
+      <button onClick={fetchRandomQuote}>Fetch Random Quote</button>
+      <button onClick={addQuoteToList}>Add to List</button>
+      <div>
+        <h2>Current Quote:</h2>
+        <p>{quote}</p>
+      </div>
+      <div>
+        <h2>List of Fetched Quotes:</h2>
+        <ul>
+          {quoteList.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
---
+++ src/Task.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Task = () => {
+  const [quoteList, setQuoteList] = useState([]);
+  const [intervalTime, setIntervalTime] = useState(5); // Initial interval time in seconds
+  const [apiCalls, setApiCalls] = useState(0);
+
+  const fetchRandomQuote = async () => {
+    try {
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      const randomQuote = response.data.slip.advice;
+      setQuoteList((prevList) => [...prevList, randomQuote]);
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+    }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (apiCalls < 15 && intervalTime <= 120) {
+        fetchRandomQuote();
+        setApiCalls(apiCalls + 1);
+
+        if (apiCalls === 4) {
+          // After every 5 API calls, double the interval time
+          setIntervalTime(intervalTime * 2);
+          setApiCalls(0);
+        }
+      } else {
+        clearInterval(intervalId); // Stop polling after 2 minutes
+      }
+    }, intervalTime * 1000); // Convert intervalTime to milliseconds
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [apiCalls, intervalTime]);
+
+  return (
+    <div>
+      <h1>Random Advice Generator</h1>
+      <div>
+        <h2>List of Fetched Quotes:</h2>
+        <ul>
+          {quoteList.map((quote, index) => (
+            <li key={index}>{quote}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Task;
